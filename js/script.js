212629bcb2681352f0a207e7148dd560983284c8
Carlos Eduardo <carlosnarciso2006@gmail.com>
2026-04-30 20:33:22 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//efeiito de transição entre as seções
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".reveal");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  elements.forEach(el => observer.observe(el));
+});
